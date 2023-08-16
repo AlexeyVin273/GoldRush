6f9d226bbba1e74bf8453bf7bf2bcac8036a487e
AlexeyVin273 <gentlecat83@gmail.com>
2023-08-16 14:44:10 +0600

    init: Project preparation

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ const refresh = (done) => {
 const syncServer = () => {
   server.init({
     server: 'build/',
-    index: 'sitemap.html',
+    index: 'index.html',
     notify: false,
     open: true,
     cors: true,
